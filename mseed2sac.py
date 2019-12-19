8085d3ec4015e434de6a849700b139e6e68d08aa
Pablo <pizutti@gmail.com>
2019-12-19 13:58:10 -0300

    Update mseed2sac.py
    xonvert minissed files to sac with correct start and end times

--- mseed2sac.py
+++ mseed2sac.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Sep 19 11:09:46 2019
 
-@author: AFC
+@author: Pablo Pizutti
 """
 
 import obspy
@@ -38,11 +38,9 @@ lastDay = ehe['day'].max()
 
 for index, file in ehe.iterrows():
     if firstDay < file['day'] < lastDay:
-        temp = ehe[ehe['day'] == (file['day'] - 1)].values.tolist()
-        st = obspy.read(temp[0][0], format='MSEED')
+        st = obspy.read(str(file['file']).replace(str(file['day']),str(file['day'] - 1)), format='MSEED')
         st += obspy.read(file['file'], format='MSEED')
-        temp = ehe[ehe['day'] == (file['day'] + 1)].values.tolist()
-        st += obspy.read(temp[0][0], format='MSEED')
+        st += obspy.read(str(file['file']).replace(str(file['day']),str(file['day'] + 1)), format='MSEED')
         tr = st[0]
         st.merge(method=1, fill_value=0)
         starttime = obspy.UTCDateTime(tr.stats.starttime.year,tr.stats.starttime.month,(tr.stats.starttime.day),0,0)
@@ -72,45 +70,9 @@ lastDay = ehn['day'].max()
 
 for index, file in ehn.iterrows():
     if firstDay < file['day'] < lastDay:
-        temp = ehn[ehn['day'] == (file['day'] - 1)].values.tolist()
-        st = obspy.read(temp[0][0], format='MSEED')
+        st = obspy.read(str(file['file']).replace(str(file['day']),str(file['day'] - 1)), format='MSEED')
         st += obspy.read(file['file'], format='MSEED')
-        temp = ehn[ehn['day'] == (file['day'] + 1)].values.tolist()
-        st += obspy.read(temp[0][0], format='MSEED')
-        tr = st[0]
-        st.merge(method=1, fill_value=0)
-        starttime = obspy.UTCDateTime(tr.stats.starttime.year,tr.stats.starttime.month,(tr.stats.starttime.day),0,0)
-        starttime += 86400
-        endtime = starttime + 86400
-        st.trim(starttime= starttime, endtime=endtime)
-        tr = st[0]
-        name = "{:02d}".format(tr.stats.starttime.year - 2000) + "{:02d}".format(tr.stats.starttime.month)
-        name += "{:02d}".format(tr.stats.starttime.day) + "00"
-        folder = "./" + str(tr.stats.starttime.year) + "_" + str(tr.stats.starttime.month)
-        print(name)
-        if not os.path.exists(folder + '/EHE'):
-            os.makedirs(folder + '/EHE')
-        if not os.path.exists(folder + '/EHN'):
-            os.makedirs(folder + '/EHN')
-        if not os.path.exists(folder + '/EHZ'):
-            os.makedirs(folder + '/EHZ')
-        if 'EHE' in file['file']:
-            st.write(folder + '/EHE/' + name + '.sac', format='SAC')
-        if 'EHN' in file['file']:
-            st.write(folder + '/EHN/' + name + '.sac', format='SAC')
-        if 'EHZ' in file['file']:
-            st.write(folder + '/EHZ/' + name + '.sac', format='SAC')
-            
-firstDay = ehz['day'].min()
-lastDay = ehz['day'].max()
-            
-for index, file in ehz.iterrows():
-    if firstDay < file['day'] < lastDay:
-        temp = ehz[ehz['day'] == (file['day'] - 1)].values.tolist()
-        st = obspy.read(temp[0][0], format='MSEED')
-        st += obspy.read(file['file'], format='MSEED')
-        temp = ehz[ehz['day'] == (file['day'] + 1)].values.tolist()
-        st += obspy.read(temp[0][0], format='MSEED')
+        st += obspy.read(str(file['file']).replace(str(file['day']),str(file['day'] + 1)), format='MSEED')
         tr = st[0]
         st.merge(method=1, fill_value=0)
         starttime = obspy.UTCDateTime(tr.stats.starttime.year,tr.stats.starttime.month,(tr.stats.starttime.day),0,0)
@@ -134,24 +96,35 @@ for index, file in ehz.iterrows():
             st.write(folder + '/EHN/' + name + 'N.sac', format='SAC')
         if 'EHZ' in file['file']:
             st.write(folder + '/EHZ/' + name + 'Z.sac', format='SAC')
-        
-#    print(file)
-#    st = obspy.read(file, format='MSEED')
-#    st.merge(method=1, fill_value=0)
-#    tr = st[0]
-#    name = "{:02d}".format(tr.stats.starttime.year - 2000) + "{:02d}".format(tr.stats.starttime.month)
-#    name += "{:02d}".format(tr.stats.starttime.day) + "00"
-#    folder = "./" + str(tr.stats.starttime.year) + "_" + str(tr.stats.starttime.month)
-#    print(name)
-#    if not os.path.exists(folder + '/EHE'):
-#        os.makedirs(folder + '/EHE')
-#    if not os.path.exists(folder + '/EHN'):
-#        os.makedirs(folder + '/EHN')
-#    if not os.path.exists(folder + '/EHZ'):
-#        os.makedirs(folder + '/EHZ')
-#    if 'EHE' in file:
-#        st.write(folder + '/EHE/' + name + 'E.sac', format='SAC')
-#    if 'EHN' in file:
-#        st.write(folder + '/EHN/' + name + 'N.sac', format='SAC')
-#    if 'EHZ' in file:
-#        st.write(folder + '/EHZ/' + name + 'Z.sac', format='SAC')
+            
+firstDay = ehz['day'].min()
+lastDay = ehz['day'].max()
+            
+for index, file in ehz.iterrows():
+    if firstDay < file['day'] < lastDay:
+        st = obspy.read(str(file['file']).replace(str(file['day']),str(file['day'] - 1)), format='MSEED')
+        st += obspy.read(file['file'], format='MSEED')
+        st += obspy.read(str(file['file']).replace(str(file['day']),str(file['day'] + 1)), format='MSEED')
+        tr = st[0]
+        st.merge(method=1, fill_value=0)
+        starttime = obspy.UTCDateTime(tr.stats.starttime.year,tr.stats.starttime.month,(tr.stats.starttime.day),0,0)
+        starttime += 86400
+        endtime = starttime + 86400
+        st.trim(starttime= starttime, endtime=endtime)
+        tr = st[0]
+        name = "{:02d}".format(tr.stats.starttime.year - 2000) + "{:02d}".format(tr.stats.starttime.month)
+        name += "{:02d}".format(tr.stats.starttime.day) + "00"
+        folder = "./" + str(tr.stats.starttime.year) + "_" + str(tr.stats.starttime.month)
+        print(name)
+        if not os.path.exists(folder + '/EHE'):
+            os.makedirs(folder + '/EHE')
+        if not os.path.exists(folder + '/EHN'):
+            os.makedirs(folder + '/EHN')
+        if not os.path.exists(folder + '/EHZ'):
+            os.makedirs(folder + '/EHZ')
+        if 'EHE' in file['file']:
+            st.write(folder + '/EHE/' + name + 'E.sac', format='SAC')
+        if 'EHN' in file['file']:
+            st.write(folder + '/EHN/' + name + 'N.sac', format='SAC')
+        if 'EHZ' in file['file']:
+            st.write(folder + '/EHZ/' + name + 'Z.sac', format='SAC')
